0206822725b7d137c1b174f0b9708f5efb4b9a23
Polo <artem-gribanof@yandex.ru>
2022-12-09 16:56:52 +0300

    первый код, проблемы

--- src/App.js
+++ src/App.js
@@ -1,5 +1,122 @@
-function App() {
-	return <div className="App">123</div>;
+import React from "react";
+import Joi from "joi";
+
+const taskSchema = Joi.array().items(
+	Joi.object({
+		id: Joi.number(),
+		value: Joi.string(),
+		state: Joi.number().integer().min(0).max(3),
+	})
+);
+
+const LOCALNAME = "__taskArray";
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		try {
+			this.state = { taskArray: Joi.attempt(JSON.parse(localStorage[LOCALNAME]), taskSchema) };
+		} catch (e) {
+			this.state = { taskArray: [] };
+		}
+
+		const idList = this.state.taskArray.map((taskObj) => taskObj.id);
+		this.maxId = idList.length ? Math.max(...idList) : -1;
+		this.setNewTask = this.setNewTask.bind(this);
+		console.log(this.state.taskArray);
+	}
+
+	// setNewTask(taskValue) {
+	// 	const taskObj = {
+	// 		id: ++this.maxId,
+	// 		value: taskValue,
+	// 		state: 0,
+	// 	};
+	// 	this.setState({ taskArray: [...this.state.taskArray, taskObj] });
+	// }
+
+	setNewTask(taskValue) {
+		const taskObj = {
+			id: ++this.maxId,
+			value: taskValue,
+			state: 0,
+		};
+		this.setState((state) => {
+			const taskArray = [...state.taskArray, taskObj];
+			localStorage.setItem(LOCALNAME, JSON.stringify(taskArray));
+			return { taskArray };
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<HeaderControl setNewTask={this.setNewTask} />
+				<TaskList taskArray={this.state.taskArray} />
+			</div>
+		);
+	}
+}
+
+class HeaderControl extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: "",
+		};
+
+		this.handleInput = this.handleInput.bind(this);
+		this.handleForm = this.handleForm.bind(this);
+	}
+
+	handleInput(e) {
+		this.setState({
+			value: e.target.value,
+		});
+	}
+
+	handleForm(e) {
+		e.preventDefault();
+		this.props.setNewTask(this.state.value);
+	}
+
+	render() {
+		return (
+			<form className="todo-header" onSubmit={this.handleForm}>
+				<input type="text" onChange={this.handleInput} value={this.state.value} />
+				<button type="submit">Добавить</button>
+			</form>
+		);
+	}
+}
+
+class TaskList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.taskArray = this.props.taskArray.map((taskObj) => <TaskItem key={taskObj.id} state={taskObj.state} value={taskObj.value} />);
+	}
+
+	render() {
+		return <div className="task-list">{this.taskArray}</div>;
+	}
+}
+
+class TaskItem extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <li>{this.props.value}</li>;
+	}
 }
 
 export default App;
+
+/*
+
+[
+	{id: 0, value: 'aboba', state: 0}
+]
+
+*/
