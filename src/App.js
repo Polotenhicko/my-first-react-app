3452f4641fca772d246037c37e1427af3498b045
Polo <artem-gribanof@yandex.ru>
2022-12-13 21:31:40 +0300

    разложил по папкам

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Joi from 'joi';
+import { HeaderControl } from './components/HeaderControl';
+import { TaskList } from './components/TaskList';
+import { LOCALNAME } from './constant';
 
 const taskSchema = Joi.array().items(
   Joi.object({
@@ -9,13 +12,7 @@ const taskSchema = Joi.array().items(
   })
 );
 
-const LOCALNAME = '__taskArray';
-const STATELIST = {
-  0: 'active',
-  1: 'completed',
-};
-
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     try {
@@ -74,138 +71,6 @@ class App extends React.Component {
     );
   }
 }
-
-class HeaderControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-    };
-  }
-
-  handleInput = (e) => {
-    this.setState({
-      value: e.target.value,
-    });
-  };
-
-  handleForm = (e) => {
-    e.preventDefault();
-    if (this.state.value) this.props.onSetNewTask(this.state.value);
-  };
-
-  render() {
-    return (
-      <form className="todo-header" onSubmit={this.handleForm}>
-        <input
-          className="todo-input"
-          type="text"
-          onChange={this.handleInput}
-          value={this.state.value}
-        />
-        <button className="todo-submit" type="submit">
-          Добавить
-        </button>
-      </form>
-    );
-  }
-}
-
-function TaskList(props) {
-  const taskArray = props.taskArray.map((taskObj) => (
-    <TaskItem
-      key={taskObj.id}
-      taskObj={taskObj}
-      onCompleteTask={props.onCompleteTask}
-      onDeleteTask={props.onDeleteTask}
-    />
-  ));
-  return <ul className="task-list">{taskArray}</ul>;
-}
-
-class TaskItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      deleted: false,
-      slideArray: [],
-    };
-    this.nextSlideId = 2;
-    // мс
-    this.transitionTime = 300;
-  }
-
-  handleTaskComplete = (e) => {
-    this.setState((state) => {
-      const nextId = this.nextSlideId++;
-      return {
-        slideArray: [
-          ...state.slideArray,
-          <SlideBackground key={nextId} state={this.props.taskObj.state} id={nextId} />,
-        ],
-      };
-    });
-    this.props.onCompleteTask(this.props.taskObj);
-  };
-
-  handleTaskDelete = (e) => {
-    e.stopPropagation();
-    this.setState({ deleted: true });
-    setTimeout(() => this.props.onDeleteTask(this.props.taskObj.id), this.transitionTime);
-  };
-
-  render() {
-    return (
-      <li
-        className={`task-item ${STATELIST[this.props.taskObj.state]} ${
-          this.state.deleted ? 'deleted' : ''
-        }`}
-        style={{
-          transition: this.state.deleted ? `opacity ${this.transitionTime}ms` : 'inherit',
-        }}
-        onClick={this.handleTaskComplete}
-      >
-        <div className="task-item-value">{this.props.taskObj.value}</div>
-        <div className="btn-task" onClick={this.handleTaskDelete}>
-          <SvgTrashCan />
-        </div>
-        {!!this.state.slideArray.length && this.state.slideArray}
-      </li>
-    );
-  }
-}
-
-class SlideBackground extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div
-        className={`slide-background ${STATELIST[this.props.state]}`}
-        style={{ zIndex: this.props.id }}
-      ></div>
-    );
-  }
-}
-
-function SvgTrashCan(props) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="489.74px"
-      height="489.74px"
-      viewBox="0 0 489.74 489.74"
-    >
-      <path d="M361.051,199.929H102.01V489.74h259.041V199.929L361.051,199.929z M170.818,450.163h-13.492V239.505h13.492V450.163z     M238.276,450.163h-13.492V239.505h13.492V450.163z M305.734,450.163h-13.492V239.505h13.492V450.163z" />
-      <path d="M387.73,145.959l-52.74-30.672l28.129-48.365L248.047,0l-28.127,48.362l-56.113-32.634l-26.678,45.875l223.922,130.231    L387.73,145.959z M257.808,36.891l68.421,39.792l-14.564,25.038L243.241,61.93L257.808,36.891z" />
-    </svg>
-  );
-}
-
-export default App;
-
 /*
 
 [
