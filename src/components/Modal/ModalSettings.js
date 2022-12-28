eed84e4ff77775f9c210499a2fadaee646f212a5
Polo <artem-gribanof@yandex.ru>
2022-12-28 23:56:55 +0300

    немного подредачил модалку

--- src/components/Modal/ModalSettings.js
+++ src/components/Modal/ModalSettings.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { SvgClose } from '../../assets/icons/SvgClose';
 
 export class ModalSettings extends React.Component {
-  modalWrapper = React.createRef();
+  modal = React.createRef();
 
   componentDidMount() {
-    this.timerAddActive = setTimeout(() => this.modalWrapper.current.classList.add('active'));
+    this.timerAddActive = setTimeout(() => this.modal.current.classList.add('active'));
   }
 
   componentWillUnmount() {
@@ -15,15 +15,15 @@ export class ModalSettings extends React.Component {
 
   onCloseModal = (e) => {
     if (e.target == e.currentTarget || e.target.closest('svg')?.hasAttribute('data-modal-close')) {
-      this.modalWrapper.current.classList.remove('active');
+      this.modal.current.classList.remove('active');
       this.timerRemoveActive = setTimeout(() => this.props.onCloseModal(), 400);
     }
   };
 
   render() {
     return (
-      <div className="modal" onClick={this.onCloseModal}>
-        <div className="modal-wrapper" ref={this.modalWrapper}>
+      <div className="modal" onClick={this.onCloseModal} ref={this.modal}>
+        <div className="modal-wrapper">
           <div className="modal-header">
             <h2 className="modal-title">Настройка</h2>
             <SvgClose />
